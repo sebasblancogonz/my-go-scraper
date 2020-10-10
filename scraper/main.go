3acafc1c7eb2b4ba5aaac8c19cc25d2a3ad41e75
Sebastian Blanco <sebastianblancogonz@gmail.com>
2020-10-10 22:23:01 +0200

    :sparkles: Add image url and isOffer to product struct

--- scraper/main.go
+++ scraper/main.go
@@ -14,8 +14,10 @@ import (
 
 //Product struct
 type Product struct {
-	Name  string
-	Price float32
+	Name    string
+	Price   float32
+	IsOffer bool
+	Image   string
 }
 
 //Category struct
@@ -34,23 +36,20 @@ func main() {
 	var products []Product
 
 	for _, category := range categories {
+		directory := "../data/carrefour/" + category.Name
 		url := host + category.URL
 		subCategories := GetAllCategories(url)
-		if _, err := os.Stat("../data/carrefour/" + category.Name); os.IsNotExist(err) {
-			os.MkdirAll("../data/carrefour/"+category.Name, 0700)
+		if _, err := os.Stat(directory); os.IsNotExist(err) {
+			os.MkdirAll(directory, 0700)
 		}
 		for _, subCategory := range subCategories {
-			if _, err := os.Stat("../data/carrefour/" + category.Name + "/" + subCategory.Name); os.IsNotExist(err) {
-				os.MkdirAll("../data/carrefour/"+category.Name+"/"+subCategory.Name, 0700)
+			subDirectory := directory + "/" + subCategory.Name
+			if _, err := os.Stat(subDirectory); os.IsNotExist(err) {
+				os.MkdirAll(subDirectory, 0700)
 			}
 			subSubCategories := GetAllCategories(host + subCategory.URL)
 			if len(subSubCategories) != 0 {
 				for _, subSubCategory := range subSubCategories {
-
-					if _, err := os.Stat("../data/carrefour/" + category.Name + "/" + subCategory.Name +
-						"/" + subSubCategory.Name); os.IsNotExist(err) {
-						os.MkdirAll("../data/carrefour/"+category.Name+"/"+subCategory.Name, 0700)
-					}
 					pages := GetProductPages(url + subSubCategory.URL)
 
 					for _, page := range pages {
@@ -59,8 +58,7 @@ func main() {
 
 					file, _ := json.MarshalIndent(products, "", "")
 
-					er := ioutil.WriteFile("../data/carrefour/"+category.Name+
-						"/"+subCategory.Name+"/"+subSubCategory.Name+".json", file, 0644)
+					er := ioutil.WriteFile(subDirectory+"/"+subSubCategory.Name+".json", file, 0644)
 					if er != nil {
 						print(er.Error())
 					}
@@ -75,7 +73,7 @@ func main() {
 
 				file, _ := json.MarshalIndent(products, "", "")
 
-				er := ioutil.WriteFile("../data/carrefour/"+category.Name+"/"+subCategory.Name+".json", file, 0644)
+				er := ioutil.WriteFile(subDirectory+".json", file, 0644)
 				if er != nil {
 					print(er.Error())
 				}
@@ -89,6 +87,7 @@ func main() {
 
 //MakeRequest Makes request
 func MakeRequest(url string) []Product {
+	emptyProduct := Product{}
 	resp, err := http.Get(url)
 
 	if err != nil {
@@ -102,9 +101,11 @@ func MakeRequest(url string) []Product {
 	if resp.StatusCode == 200 {
 		doc, _ := goquery.NewDocumentFromReader(resp.Body)
 		doc.Find(".product-card-item").Each(func(i int, s *goquery.Selection) {
-			if s.Find("title-product").Text() != "" {
-				products = append(products, CreateProduct(s))
+			p := CreateProduct(s)
+			if p != emptyProduct {
+				products = append(products, p)
 			}
+
 		})
 
 		return products
@@ -115,17 +116,23 @@ func MakeRequest(url string) []Product {
 //CreateProduct creates products
 func CreateProduct(s *goquery.Selection) Product {
 	var price float32
+	var isOffer bool
 	name := strings.Replace(s.Find(".title-product").Text(), "\n", "", -1)
-	if CheckIfPriceLess(name, s.Find(".price").Text()) {
-		rawPrice := strings.Replace(s.Find(".price").Text(), ",", ".", 1)
-		price = ConvertString(rawPrice)
+	if name != "" {
+		imgURL, _ := s.Find("img").Attr("src")
+		if CheckIfPriceLess(name, s.Find(".price").Text()) {
+			rawPrice := strings.Replace(s.Find(".price").Text(), ",", ".", 1)
+			price = ConvertString(rawPrice)
+		}
+		if CheckIfPriceLess(name, s.Find(".price-less").Text()) {
+			rawPrice := strings.Replace(s.Find(".price-less").Text(), ",", ".", 1)
+			delLineBreaks := strings.Replace(rawPrice, "\n", "", 1)
+			price = ConvertString(delLineBreaks)
+			isOffer = true
+		}
+		return Product{Name: name, Price: price, IsOffer: isOffer, Image: imgURL}
 	}
-	if CheckIfPriceLess(name, s.Find(".price-less").Text()) {
-		rawPrice := strings.Replace(s.Find(".price-less").Text(), ",", ".", 1)
-		delLineBreaks := strings.Replace(rawPrice, "\n", "", 1)
-		price = ConvertString(delLineBreaks)
-	}
-	return Product{Name: name, Price: price}
+	return Product{}
 }
 
 //ConvertString converts strings to float32
